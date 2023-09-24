855e98cc912879fe599fc7504f1dff3530efe845
demetere <d.dzmansahvili@cellectenergy.com>
2023-09-24 19:48:01 +0200

    fix requirements

--- setup.py
+++ setup.py
@@ -28,8 +28,9 @@ setuptools.setup(
         'torch>=1.8.1',
         'gym<=0.22.0',  # PFRL needs an update for future versions
         'pfrl>=0.2.1',
-        'traci=1.18.0',
-        'libsumo==1.18.0'
+        'traci==1.18.0',
+        'libsumo==1.18.0',
+        'protobuf==3.20.*'
     ],
     extras_require={
         "fma2c": ['tensorflow==1.15.5'],
