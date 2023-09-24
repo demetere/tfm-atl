1b55ef009ffd5660029db4c352dc9134adbe563d
demetere <d.dzmansahvili@cellectenergy.com>
2023-09-24 12:04:41 +0200

    added phase pairs and valid acts

--- resco_benchmark/config/agent_config.py
+++ resco_benchmark/config/agent_config.py
@@ -92,7 +92,7 @@ agent_configs = {
         'state': states.drq_norm,
         'reward': rewards.wait_norm,
         'max_distance': 200,
-        'save_freq': 2,
+        'save_freq': 5,
         'BATCH_SIZE': 32,
         'GAMMA': 0.99,
         'EPS_START': 1.0,
