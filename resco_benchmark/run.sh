1cd6683efb2273e9c1b7a29c53d43f659ff27646
demetere <d.dzmansahvili@cellectenergy.com>
2023-09-25 15:31:48 +0200

    fix shell scripts

--- resco_benchmark/run.sh
+++ resco_benchmark/run.sh
@@ -1,19 +1,24 @@
 export LIBSUMO_AS_TRACI=1
-export TFM_RESCO_PWD=/home/demetere/Projects/tfm-atl/resco_benchmark
-export TFM_RESCO_LOG_DIR=/home/demetere/Projects/tfm-atl/resco_benchmark/results/
-
+export TFM_RESCO_PWD=$PWD
+export TFM_RESCO_LOG_DIR="$PWD/results/"
 
 export TFM_RESCO_EPS=1500
-export TFM_RESCO_TR=1
+export TFM_RESCO_TR=2
 
 # HOW TO RUN: train/train.sh AGENT TRIES
 
-train/train.sh STOCHASTIC $TFM_RESCO_TR
-train/train.sh MAXWAVE $TFM_RESCO_TR
-train/train.sh MAXPRESSURE $TFM_RESCO_TR
-train/train.sh IDQN $TFM_RESCO_TR
-train/train.sh IPPO $TFM_RESCO_TR
-train/train.sh MPLIGHT $TFM_RESCO_TR
-train/train.sh MPLIGHTFULL $TFM_RESCO_TR
-train/train.sh FMA2C $TFM_RESCO_TR
-train/train.sh FMA2CFULL $TFM_RESCO_TR
+TRIES_COUNTER=0
+while [ $TRIES_COUNTER -le $((TFM_RESCO_TR - 1)) ]
+do
+    train/train.sh STOCHASTIC $TRIES_COUNTER
+    train/train.sh MAXWAVE $TRIES_COUNTER
+    train/train.sh MAXPRESSURE $TRIES_COUNTER
+    train/train.sh IDQN $TRIES_COUNTER
+    train/train.sh IPPO $TRIES_COUNTER
+    train/train.sh MPLIGHT $TRIES_COUNTER
+    train/train.sh MPLIGHTFULL $TRIES_COUNTER
+    train/train.sh FMA2C $TRIES_COUNTER
+    train/train.sh FMA2CFULL $TRIES_COUNTER
+    echo "RUN $((++TRIES_COUNTER))/$TFM_RESCO_TR FINISHED"
+done
+
--- resco_benchmark/train/train.sh
+++ resco_benchmark/train/train.sh
@@ -2,18 +2,13 @@
 AGENT_NAME=$1
 TRIES=$2
 
-TRIES_COUNTER=0
-while [ $TRIES_COUNTER -le $((TRIES - 1)) ]
-do
-    python main.py \
-        --agent $AGENT_NAME \
-        --map vake \
-        --libsumo 1 \
-        --tr $TRIES_COUNTER \
-        --eps $TFM_RESCO_EPS \
-        --pwd $TFM_RESCO_PWD \
-        --log_dir $TFM_RESCO_LOG_DIR
-    echo RUN $((++TRIES_COUNTER))/$TRIES FINISHED
-done
+python main.py \
+    --agent $AGENT_NAME \
+    --map vake \
+    --libsumo 1 \
+    --tr $TRIES \
+    --eps $TFM_RESCO_EPS \
+    --pwd $TFM_RESCO_PWD \
+    --log_dir $TFM_RESCO_LOG_DIR
 
 train/analyses.sh $AGENT_NAME
