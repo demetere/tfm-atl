e27435c04d868c00ba5aede04e3eaf70fb27f90f
demetere <ddzmanashvili@raizomat.com>
2023-09-25 17:21:15 +0400

    update shell files

--- resco_benchmark/run.sh
+++ resco_benchmark/run.sh
@@ -1,19 +1,19 @@
 export LIBSUMO_AS_TRACI=1
-export TFM_RESCO_PWD=~/Projects/mai/traffic-control/tfm-atl/resco_benchmark
-export TFM_RESCO_LOG_DIR=~/Projects/mai/traffic-control/tfm-atl/resco_benchmark/results/
+export TFM_RESCO_PWD=/home/demetere/Projects/tfm-atl/resco_benchmark
+export TFM_RESCO_LOG_DIR=/home/demetere/Projects/tfm-atl/resco_benchmark/results/
 
 
-export TFM_RESCO_EPS=5
-TFM_RESCO_TR=1
+export TFM_RESCO_EPS=1500
+export TFM_RESCO_TR=1
 
 # HOW TO RUN: train/train.sh AGENT TRIES
 
-# train/train.sh STOCHASTIC 1
-# train/train.sh MAXWAVE 1
-# train/train.sh MAXPRESSURE 1
-# train/train.sh IDQN $TFM_RESCO_TR
-# train/train.sh IPPO $TFM_RESCO_TR
-# train/train.sh MPLIGHT $TFM_RESCO_TR
-# train/train.sh MPLIGHTFULL $TFM_RESCO_TR
-# train/train.sh FMA2C $TFM_RESCO_TR
-# train/train.sh FMA2CFULL $TFM_RESCO_TR
+train/train.sh STOCHASTIC $TFM_RESCO_TR
+train/train.sh MAXWAVE $TFM_RESCO_TR
+train/train.sh MAXPRESSURE $TFM_RESCO_TR
+train/train.sh IDQN $TFM_RESCO_TR
+train/train.sh IPPO $TFM_RESCO_TR
+train/train.sh MPLIGHT $TFM_RESCO_TR
+train/train.sh MPLIGHTFULL $TFM_RESCO_TR
+train/train.sh FMA2C $TFM_RESCO_TR
+train/train.sh FMA2CFULL $TFM_RESCO_TR
--- resco_benchmark/train/git.sh
+++ resco_benchmark/train/git.sh
@@ -3,5 +3,5 @@ AGENT=$1
 
 git pull
 git add --ignore-errors utils/*
-git commit -m "updated results for $AGENT, eps: $TFM_RESCO_EPS"
+git commit -m "FROM SHELL. AGENT: $AGENT, EPS: $TFM_RESCO_EPS, TRIES: $TFM_RESCO_TR"
 git push
