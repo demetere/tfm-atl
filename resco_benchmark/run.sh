0e655100733931114b4b2a1563fab5453b8bf22c
Demetre Dzmanashvili <demetredzmanashvili@gmail.com>
2023-10-01 21:56:43 +0200

    modified again

--- resco_benchmark/run.sh
+++ resco_benchmark/run.sh
@@ -13,17 +13,17 @@ while [ $TRIES_COUNTER -le $((TFM_RESCO_TR - 1)) ]
 do
     if [ $TRIES_COUNTER -ne 0 ]; then # Because we already ran one run
         train/train.sh IDQN $TRIES_COUNTER
+        train/train.sh IPPO $TRIES_COUNTER
     fi
 
-    train/train.sh IPPO $TRIES_COUNTER
-    train/train.sh MPLIGHT $TRIES_COUNTER
-    train/train.sh MPLIGHTFULL $TRIES_COUNTER
+    train/train.sh MPLight $TRIES_COUNTER
+    train/train.sh MPLightFULL $TRIES_COUNTER
     train/train.sh FMA2C $TRIES_COUNTER
     train/train.sh FMA2CFULL $TRIES_COUNTER
 
     if [ $TRIES_COUNTER -ne 0 ]; then # Because we already ran one run
         train/train.sh STOCHASTIC $TRIES_COUNTER
-	train/train.sh MAXWAVE $TRIES_COUNTER
+	    train/train.sh MAXWAVE $TRIES_COUNTER
         train/train.sh MAXPRESSURE $TRIES_COUNTER
     fi
 
