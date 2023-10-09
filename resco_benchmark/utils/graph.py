9c3613b74332674ab5667d7b59bfd96de9d2dd30
Demetre Dzmanashvili <demetredzmanashvili@gmail.com>
2023-10-09 12:45:07 +0200

    update graph logic

--- resco_benchmark/utils/graph.py
+++ resco_benchmark/utils/graph.py
@@ -19,15 +19,17 @@ alg_name = {
     'MPLight': 'MPLight',
     'MPLightFULL': 'Full State MPLight',
     'FMA2C': 'FMA2C',
+    'FMA2CFULL': 'Full State FMA2C',
     'IPPO': 'IPPO'
 }
 
 statics = ['MAXPRESSURE', 'STOCHASTIC', 'MAXWAVE', 'FIXED']
 
 num_n = -1
-num_episodes = 1500
-fs = 15
-window_size = 25
+NUM_EPISODES = 1500
+FONT_SIZE = 15
+AVERAGE_WINDOW_SIZE = 25
+FIGURE_SIZE = (15,8)
 
 metrics = [delays, durations, waiting, queue]
 metrics_str = ['Avg. Delay', 'Avg. Trip Time', 'Avg. Wait', 'Avg. Queue']
@@ -66,19 +68,18 @@ chart = {
 }
 
 for met_i, metric in enumerate(metrics):
-    plt.figure(figsize=(15, 15))
+    plt.figure(figsize=FIGURE_SIZE)
     print('\n', metrics_str[met_i])
     for map in map_title.keys():
         print()
         print(map_title[map])
-        dqn_max = 0
         plt.gca().set_prop_cycle(None)
         for key in metric:
             if map in key and '_yerr' not in key:
                 alg = key.split(' ')[0]
+                if 'full' in key: alg += 'FULL'
+                
                 key_map = key.split(' ')[1]
-
-                if alg == 'IDQN': dqn_max = np.max(metric[key])     # Set ylim to DQN max, it's approx. random perf.
 
                 # Print out performance metric
                 err = metric.get(key + '_yerr')
@@ -107,13 +108,13 @@ for met_i, metric in enumerate(metrics):
 
                 # Build plots
                 if alg in statics:
-                    plt.plot([avg_tot]*num_episodes, '--', label=alg_name[alg])
+                    plt.plot([avg_tot]*NUM_EPISODES, '--', label=alg_name[alg])
                     plt.fill_between([], [], [])      # Advance color cycle
                 # elif not('FMA2C' in alg or 'IPPO' in alg):
                 else:
                     windowed = []
-                    queue = deque(maxlen=window_size)
-                    std_q = deque(maxlen=window_size)
+                    queue = deque(maxlen=AVERAGE_WINDOW_SIZE)
+                    std_q = deque(maxlen=AVERAGE_WINDOW_SIZE)
 
                     windowed_yerr = []
                     x = []
@@ -140,22 +141,22 @@ for met_i, metric in enumerate(metrics):
                 #     if alg == 'FMA2C':  # Skip pink in color cycle
                 #         plt.plot([], [])
                 #         plt.fill_between([], [], [])
-                #     x = [num_episodes-1, num_episodes]
+                #     x = [NUM_EPISODES-1, NUM_EPISODES]
                 #     y = [last_n]*2
                 #     plt.plot(x, y, label=alg_name[alg])
                 #     plt.fill_between([], [], [])  # Advance color cycle
 
-        points = np.asarray([0, 20, 40, 60, 80, 100, num_episodes])
-        labels = ('0', '20', '40', '60', '80', '100', str(num_episodes))
-        plt.yticks(fontsize=fs)
-        plt.xticks(points, labels, fontsize=fs)
-        plt.xlabel('Episode', fontsize=fs)
-        plt.ylabel(f'{metrics_str[met_i]}', fontsize=fs)
-        plt.title(map_title[map], fontsize=fs)
-        plt.legend(prop={'size': fs})
+        points = np.asarray([0, 200, 400, 600, 800, 1000, NUM_EPISODES])
+        labels = ('0', '200', '400', '600', '800', '1000', str(NUM_EPISODES))
+        plt.yticks(fontsize=FONT_SIZE)
+        plt.xticks(points, labels, fontsize=FONT_SIZE)
+        plt.xlabel('Episode', fontsize=FONT_SIZE)
+        plt.ylabel(f'{metrics_str[met_i]}', fontsize=FONT_SIZE)
+        plt.title(map_title[map], fontsize=FONT_SIZE)
+        plt.legend(prop={'size': FONT_SIZE})
         bot, top = plt.ylim()
         if bot < 0: bot = 0
-        # plt.ylim(bot, dqn_max)
+        
         plt.show()
 
 for alg in chart:
